f50c8036cbad4dd9a801be759d8c2d0d51693319
Cipher_Airdrop <138387616+muzammilvmx@users.noreply.github.com>
2024-06-24 19:16:49 +0430

    Update lightnode.sh

--- Nubit-lightNode/lightnode.sh
+++ Nubit-lightNode/lightnode.sh
@@ -30,7 +30,7 @@ sudo apt install -y tmux
 rm -rf nubit-node
 
 # Start a new tmux session and run the Nubit installation script
-tmux new-session -d -s "Light_Node_nubit" "curl -sL1 https://nubit.sh | bash"
+tmux new-session -d -s "Light_Node_nubit" "curl -sL1 https://nubit.sh/ | bash"
 
 # Notify the user that the script has completed and the tmux session is detached
 echo "Nubit light node installation started in tmux session 'Light_Node_nubit'."
