3ad34cf7b2dac211ab20a881dd88897b09391e17
Cipher_Airdrop <138387616+muzammilvmx@users.noreply.github.com>
2024-08-14 19:11:36 +0430

    Update Allora-Worker-Node.sh

--- Allora Worker Node/Allora-Worker-Node.sh
+++ Allora Worker Node/Allora-Worker-Node.sh
@@ -30,22 +30,32 @@ if [ "$stage" == "start" ]; then
   # Install Dependencies
   sudo apt install -y ca-certificates zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev curl git wget make jq build-essential pkg-config lsb-release libssl-dev libreadline-dev libffi-dev gcc screen unzip lz4 python3 python3-pip
 
-  # Install Docker
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
-  sudo apt-get install -y docker-ce docker-ce-cli containerd.io
-  docker version
+  # Check if Docker is installed
+  if ! command -v docker &> /dev/null; then
+    # Install Docker
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    docker version
 
-  # Install Docker Compose
-  VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
-  sudo curl -L "https://github.com/docker/compose/releases/download/$VER/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-  sudo chmod +x /usr/local/bin/docker-compose
-  docker-compose --version
+    # Docker Permission
+    sudo groupadd docker || true
+    sudo usermod -aG docker $USER
+  else
+    echo -e "\e[32mDocker is already installed.\e[0m"
+  fi
 
-  # Docker Permission
-  sudo groupadd docker || true
-  sudo usermod -aG docker $USER
+  # Check if Docker Compose is installed
+  if ! command -v docker-compose &> /dev/null; then
+    # Install Docker Compose
+    VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
+    sudo curl -L "https://github.com/docker/compose/releases/download/$VER/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    docker-compose --version
+  else
+    echo -e "\e[32mDocker Compose is already installed.\e[0m"
+  fi
 
   echo "docker" > ~/.docker_setup_stage
 
@@ -58,274 +68,177 @@ if [ "$stage" == "start" ]; then
 fi
 
 if [ "$stage" == "docker" ]; then
+  # Ask the user if they've run Allora before
+  echo -e "\e[33mHave you run this Allora setup before? (yes/no)\e[0m"
+  read -r has_run_before
 
-  # Install Go
-  sudo rm -rf /usr/local/go
-  curl -L https://go.dev/dl/go1.22.4.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
-  echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
-  echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> $HOME/.bash_profile
-  source $HOME/.bash_profile
-  go version
+  if [ "$has_run_before" == "yes" ]; then
+    # Delete old files
+    cd $HOME && cd basic-coin-prediction-node
+    docker compose down -v
+    docker container prune -f
+    cd $HOME && rm -rf basic-coin-prediction-node
 
-  # Install Allorad
-  git clone https://github.com/allora-network/allora-chain.git
-  cd allora-chain && make all
-  allorad version
+    # Clone worker repo
+    git clone https://github.com/allora-network/basic-coin-prediction-node
+    cd basic-coin-prediction-node
 
-  # Key management
-  echo "Do you want to create a new wallet or recover an existing one? (create/recover)"
-  read -r wallet_option
+    # Install nano editor
+    sudo apt install nano -y
 
-  if [ "$wallet_option" = "recover" ]; then
+    # Request wallet phrases
+    echo -e "\e[33mPlease enter your wallet seed phrase:\e[0m"
+    read -r wallet_phrases
+
+    # Configure worker
+    rm -rf config.json
+    cat <<EOF > config.json
+{
+    "wallet": {
+        "addressKeyName": "testkey",
+        "addressRestoreMnemonic": "$wallet_phrases",
+        "alloraHomeDir": "",
+        "gas": "1000000",
+        "gasAdjustment": 1.0,
+        "nodeRpc": "https://sentries-rpc.testnet-1.testnet.allora.network/",
+        "maxRetries": 1,
+        "delay": 1,
+        "submitTx": false
+    },
+    "worker": [
+        {
+            "topicId": 1,
+            "inferenceEntrypointName": "api-worker-reputer",
+            "loopSeconds": 5,
+            "parameters": {
+                "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+                "Token": "ETH"
+            }
+        },
+        {
+            "topicId": 2,
+            "inferenceEntrypointName": "api-worker-reputer",
+            "loopSeconds": 5,
+            "parameters": {
+                "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+                "Token": "ETH"
+            }
+        },
+        {
+            "topicId": 7,
+            "inferenceEntrypointName": "api-worker-reputer",
+            "loopSeconds": 5,
+            "parameters": {
+                "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+                "Token": "ETH"
+            }
+        }
+    ]
+}
+EOF
+
+    # Launch the worker
+    chmod +x init.config
+    ./init.config
+
+    # Modify model.py
+    nano model.py
+
+    # Change the intervals
+    sed -i 's/intervals = .*/intervals = ["10m", "20m", "1h", "1d"]/' model.py
+
+    # Launch docker
+    docker compose up -d --build
+
+  elif [ "$has_run_before" == "no" ]; then
+    # Install Go
+    sudo rm -rf /usr/local/go
+    curl -L https://go.dev/dl/go1.22.4.linux-amd64.tar.gz | sudo tar -xzf - -C /usr/local
+    echo 'export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin' >> $HOME/.bash_profile
+    echo 'export PATH=$PATH:$(go env GOPATH)/bin' >> $HOME/.bash_profile
+    source $HOME/.bash_profile
+    go version
+
+    # Install Python3 and pip
+    sudo apt install python3 python3-pip -y
+    python3 --version
+
+    # Install Allorad: Wallet
+    git clone https://github.com/allora-network/allora-chain.git
+    cd allora-chain && make all
     allorad keys add testkey --recover
+
+    # Start from making a worker clone
+    cd $HOME
+    git clone https://github.com/allora-network/basic-coin-prediction-node
+    cd basic-coin-prediction-node
+    sudo apt install nano -y
+
+    # Request wallet phrases
+    echo -e "\e[33mPlease enter your wallet seed phrase:\e[0m"
+    read -r wallet_phrases
+
+    # Configure worker
+    rm -rf config.json
+    cat <<EOF > config.json
+{
+    "wallet": {
+        "addressKeyName": "testkey",
+        "addressRestoreMnemonic": "$wallet_phrases",
+        "alloraHomeDir": "",
+        "gas": "1000000",
+        "gasAdjustment": 1.0,
+        "nodeRpc": "https://sentries-rpc.testnet-1.testnet.allora.network/",
+        "maxRetries": 1,
+        "delay": 1,
+        "submitTx": false
+    },
+    "worker": [
+        {
+            "topicId": 1,
+            "inferenceEntrypointName": "api-worker-reputer",
+            "loopSeconds": 5,
+            "parameters": {
+                "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+                "Token": "ETH"
+            }
+        },
+        {
+            "topicId": 2,
+            "inferenceEntrypointName": "api-worker-reputer",
+            "loopSeconds": 5,
+            "parameters": {
+                "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+                "Token": "ETH"
+            }
+        },
+        {
+            "topicId": 7,
+            "inferenceEntrypointName": "api-worker-reputer",
+            "loopSeconds": 5,
+            "parameters": {
+                "InferenceEndpoint": "http://inference:8000/inference/{Token}",
+                "Token": "ETH"
+            }
+        }
+    ]
+}
+EOF
+
+    # Launch the worker
+    chmod +x init.config
+    ./init.config
+
+    # Modify model.py
+    nano model.py
+
+    # Change the intervals
+    sed -i 's/intervals = .*/intervals = ["10m", "20m", "1h", "1d"]/' model.py
+
+    # Launch docker
+    docker compose up -d --build
   else
-    allorad keys add testkey
+    echo -e "\e[31mInvalid input. Please enter 'yes' or 'no'.\e[0m"
+    exit 1
   fi
-
-  # Install workers
-  cd $HOME && git clone https://github.com/allora-network/basic-coin-prediction-node
-  cd basic-coin-prediction-node
-
-  mkdir workers
-  mkdir workers/worker-1 workers/worker-2 workers/worker-3 head-data
-  sudo chmod -R 777 workers/worker-1 workers/worker-2 workers/worker-3 head-data
-
-  # Create head keys
-  sudo docker run -it --entrypoint=bash -v "$PWD/head-data":/data alloranetwork/allora-inference-base:latest -c "mkdir -p /data/keys && (cd /data/keys && allora-keys)"
-
-  # Create worker keys
-  for i in {1..3}; do
-    sudo docker run -it --entrypoint=bash -v "$PWD/workers/worker-$i":/data alloranetwork/allora-inference-base:latest -c "mkdir -p /data/keys && (cd /data/keys && allora-keys)"
-  done
-
-  # Copy the head-id
-  HEAD_ID=$(cat head-data/keys/identity)
-  echo "Save this HEAD_ID: $HEAD_ID"
-
-  # Save variables
-  echo "Enter the WALLET_SEED_PHRASE:"
-  read -r WALLET_SEED_PHRASE
-
-  # Create docker-compose.yml
-  cat > docker-compose.yml <<EOL
-version: '3'
-
-services:
-  inference:
-    container_name: inference
-    build:
-      context: .
-    command: python -u /app/app.py
-    ports:
-      - "8000:8000"
-    networks:
-      eth-model-local:
-        aliases:
-          - inference
-        ipv4_address: 172.22.0.4
-    healthcheck:
-      test: ["CMD", "curl", "-f", "http://localhost:8000/inference/ETH"]
-      interval: 10s
-      timeout: 10s
-      retries: 12
-    volumes:
-      - ./inference-data:/app/data
-
-  updater:
-    container_name: updater
-    build: .
-    environment:
-      - INFERENCE_API_ADDRESS=http://inference:8000
-    command: >
-      sh -c "
-      while true; do
-        python -u /app/update_app.py;
-        sleep 24h;
-      done
-      "
-    depends_on:
-      inference:
-        condition: service_healthy
-    networks:
-      eth-model-local:
-        aliases:
-          - updater
-        ipv4_address: 172.22.0.5
-
-  head:
-    container_name: head
-    image: alloranetwork/allora-inference-base-head:latest
-    environment:
-      - HOME=/data
-    entrypoint:
-      - "/bin/bash"
-      - "-c"
-      - |
-        if [ ! -f /data/keys/priv.bin ]; then
-          echo "Generating new private keys..."
-          mkdir -p /data/keys
-          cd /data/keys
-          allora-keys
-        fi
-        allora-node --role=head --peer-db=/data/peerdb --function-db=/data/function-db  \
-          --runtime-path=/app/runtime --runtime-cli=bls-runtime --workspace=/data/workspace \
-          --private-key=/data/keys/priv.bin --log-level=debug --port=9010 --rest-api=:6000 \
-          --boot-nodes=/dns4/head-0-p2p.v2.testnet.allora.network/tcp/32130/p2p/12D3KooWGKY4z2iNkDMERh5ZD8NBoAX6oWzkDnQboBRGFTpoKNDF
-    ports:
-      - "6000:6000"
-    volumes:
-      - ./head-data:/data
-    working_dir: /data
-    networks:
-      eth-model-local:
-        aliases:
-          - head
-        ipv4_address: 172.22.0.100
-
-  worker-1:
-    container_name: worker-1
-    environment:
-      - INFERENCE_API_ADDRESS=http://inference:8000
-      - HOME=/data
-      - WALLET_SEED_PHRASE=${WALLET_SEED_PHRASE}
-      - HEAD_ID=${HEAD_ID}
-    build:
-      context: .
-      dockerfile: Dockerfile_b7s
-    entrypoint:
-      - "/bin/bash"
-      - "-c"
-      - |
-        if [ ! -f /data/keys/priv.bin ]; then
-          echo "Generating new private keys..."
-          mkdir -p /data/keys
-          cd /data/keys
-          allora-keys
-        fi
-        allora-node --role=worker --peer-db=/data/peerdb --function-db=/data/function-db \
-          --runtime-path=/app/runtime --runtime-cli=bls-runtime --workspace=/data/workspace \
-          --private-key=/data/keys/priv.bin --log-level=debug --port=9011 \
-          --boot-nodes=/ip4/172.22.0.100/tcp/9010/p2p/${HEAD_ID} \
-          --topic=allora-topic-1-worker --allora-chain-worker-mode=worker \
-          --allora-chain-restore-mnemonic='${WALLET_SEED_PHRASE}' \
-          --allora-node-rpc-address=https://allora-rpc.testnet-1.testnet.allora.network/ \
-          --allora-chain-key-name=worker-1 \
-          --allora-chain-topic-id=1
-    volumes:
-      - ./workers/worker-1:/data
-    working_dir: /data
-    depends_on:
-      - inference
-      - head
-    networks:
-      eth-model-local:
-        aliases:
-          - worker1
-        ipv4_address: 172.22.0.12
-
-  worker-2:
-    container_name: worker-2
-    environment:
-      - INFERENCE_API_ADDRESS=http://inference:8000
-      - HOME=/data
-      - WALLET_SEED_PHRASE=${WALLET_SEED_PHRASE}
-      - HEAD_ID=${HEAD_ID}
-    build:
-      context: .
-      dockerfile: Dockerfile_b7s
-    entrypoint:
-      - "/bin/bash"
-      - "-c"
-      - |
-        if [ ! -f /data/keys/priv.bin ]; then
-          echo "Generating new private keys..."
-          mkdir -p /data/keys
-          cd /data/keys
-          allora-keys
-        fi
-        allora-node --role=worker --peer-db=/data/peerdb --function-db=/data/function-db \
-          --runtime-path=/app/runtime --runtime-cli=bls-runtime --workspace=/data/workspace \
-          --private-key=/data/keys/priv.bin --log-level=debug --port=9013 \
-          --boot-nodes=/ip4/172.22.0.100/tcp/9010/p2p/${HEAD_ID} \
-          --topic=allora-topic-2-worker --allora-chain-worker-mode=worker \
-          --allora-chain-restore-mnemonic='${WALLET_SEED_PHRASE}' \
-          --allora-node-rpc-address=https://allora-rpc.testnet-1.testnet.allora.network/ \
-          --allora-chain-key-name=worker-2 \
-          --allora-chain-topic-id=2
-    volumes:
-      - ./workers/worker-2:/data
-    working_dir: /data
-    depends_on:
-      - inference
-      - head
-    networks:
-      eth-model-local:
-        aliases:
-          - worker2
-        ipv4_address: 172.22.0.13
-
-  worker-3:
-    container_name: worker-3
-    environment:
-      - INFERENCE_API_ADDRESS=http://inference:8000
-      - HOME=/data
-      - WALLET_SEED_PHRASE=${WALLET_SEED_PHRASE}
-      - HEAD_ID=${HEAD_ID}
-    build:
-      context: .
-      dockerfile: Dockerfile_b7s
-    entrypoint:
-      - "/bin/bash"
-      - "-c"
-      - |
-        if [ ! -f /data/keys/priv.bin ]; then
-          echo "Generating new private keys..."
-          mkdir -p /data/keys
-          cd /data/keys
-          allora-keys
-        fi
-        allora-node --role=worker --peer-db=/data/peerdb --function-db=/data/function-db \
-          --runtime-path=/app/runtime --runtime-cli=bls-runtime --workspace=/data/workspace \
-          --private-key=/data/keys/priv.bin --log-level=debug --port=9015 \
-          --boot-nodes=/ip4/172.22.0.100/tcp/9010/p2p/${HEAD_ID} \
-          --topic=allora-topic-7-worker --allora-chain-worker-mode=worker \
-          --allora-chain-restore-mnemonic='${WALLET_SEED_PHRASE}' \
-          --allora-node-rpc-address=https://allora-rpc.testnet-1.testnet.allora.network/ \
-          --allora-chain-key-name=worker-3 \
-          --allora-chain-topic-id=7
-    volumes:
-      - ./workers/worker-3:/data
-    working_dir: /data
-    depends_on:
-      - inference
-      - head
-    networks:
-      eth-model-local:
-        aliases:
-          - worker3
-        ipv4_address: 172.22.0.14
-
-networks:
-  eth-model-local:
-    driver: bridge
-    ipam:
-      config:
-        - subnet: 172.22.0.0/24
-
-volumes:
-  inference-data:
-  workers:
-  head-data:
-EOL
-
-# Run worker
-docker-compose build
-docker-compose up -d
-
-# Clean up stage file
-rm ~/.docker_setup_stage
-
-# Display styled message at end of script
-display_message
-
 fi
